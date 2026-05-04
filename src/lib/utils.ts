afa9e534e2f60758ea8edcfc5b4a1c676893e329
Wai Yan <koyannote2180@gmail.com>
2026-05-04 09:57:10 +0700

    feat: update utils allow origins

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,7 +2,8 @@ export const ALLOWED_ORIGINS = [
   "http://localhost:8080",
   "http://localhost:3000",
   "https://predictocean-dashboard.vercel.app",
-  "https://predictocean.vercel.app"
+  "https://predictocean.vercel.app",
+  "https://www.predictocean.com"
 ];
 
 export const MATCH_DURATION_MINUTES = 180 * 60 * 1000;
